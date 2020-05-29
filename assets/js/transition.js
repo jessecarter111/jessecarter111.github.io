e2efb19fba6d44e357f1b775f38a09c2cdb587f4
Jesse <m.c.jesse@live.com>
2020-05-28 21:45:31 -0400

    test2

---
+++ assets/js/transition.js
@@ -0,0 +1,54 @@
+console.log($('.content').attr('class'));
+console.log($('body').attr('class'));
+$(document).ready(function () {
+    //setLinks();
+    reverse();
+    animate();
+});
+
+function reverse(){
+    console.log($('body').attr('class'));
+    currColor = $('body').attr('class');
+    if (currColor == 'white'){
+        $('body').removeClass('white');
+        $('.content').removeClass('black');
+
+        $('body').addClass('black');
+        $('.content').addClass('white');
+    }
+    if (currColor == 'black') {
+        $('body').removeClass('black');
+        $('.content').removeClass('white');
+
+        $('body').addClass('white');
+        $('.content').addClass('black');
+    }
+}
+
+function animate() {
+    $('.content').addClass('animate_content');
+    /*
+    $('.page_description').fadeOut(100).delay(2800).fadeIn();
+    setTimeout(function () {
+        $('.cotent').removeClass('animate_content');
+    }, 3200);
+
+    setTimeout(function () {
+        $('.content').removeClass('fadeIn');
+    }, 1500);
+    */
+}
+
+function setLinks() {
+    $('.index_link').click(function () {
+        setTimeout(function () {
+            $('.index').addClass('fadeIn');
+        }, 1500);
+    });
+
+    $('.work_link').click(function () {
+        setTimeout(function () {
+            $('.work').addClass('fadeIn');
+        }, 1500);
+    });
+}
