1a40e0805e82fcc0e33ffb5f26e12b5b5991f855
Jesse <m.c.jesse@live.com>
2020-08-12 13:35:03 -0400

    fixing server side of captcha

---
+++ ContactFrom_v3/proxy.php
@@ -0,0 +1,11 @@
+if (!isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+    header('HTTP/1.0 403 Forbidden');
+    die('You are not allowed to access this file.');     
+}
+header('Content-type: application/json');
+$url=$_GET['url'];
+$response=$_GET['response'];
+$secret = "[6LeVDb4ZAAAAAHfdWoXtd3HzkHE0JXjfcMVPd4yX]"; 
+$params = array('secret'=> $secret, 'response'=> $response);
+$json=file_get_contents( $url  . '?secret=' . $secret . '&amp;response=' . $response);
+echo $json; 
